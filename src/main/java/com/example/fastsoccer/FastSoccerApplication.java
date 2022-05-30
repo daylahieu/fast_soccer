7627719313b4aad2b56688161779bf7cb578b6aa
Mai Hieu <maiminhhieu1999@gmail.com>
2022-05-30 14:18:46 +0700

    First commit

---
+++ src/main/java/com/example/fastsoccer/FastSoccerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastsoccer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastSoccerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastSoccerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fastsoccer/FastSoccerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastsoccer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastSoccerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
