1df353abc13ad1854033ef141a500beac7ee8f9d
Mai Hieu <maiminhhieu1999@gmail.com>
2022-06-12 00:37:04 +0700

    update FE

--- src/main/java/com/example/fastsoccer/entity/Yard.java
+++ src/main/java/com/example/fastsoccer/entity/Yard.java
@@ -20,29 +20,29 @@ public class Yard {
     @Column(name = "name")
     private String name;
     @Column(name = "1h-3h")
-    private int h12;
+    private int h13;
     @Column(name = "3h-5h")
-    private int h12;
+    private int h35;
     @Column(name = "1h-7h")
-    private int h12;
+    private int h57;
     @Column(name = "1h-7h")
-    private int h12;
+    private int h79;
     @Column(name = "1h-9h")
-    private int h12;
+    private int h911;
     @Column(name = "1h-11h")
-    private int h12;
+    private int h1113;
     @Column(name = "1h-13h")
-    private int h12;
+    private int h1315;
     @Column(name = "1h-15h")
-    private int h12;
+    private int h1517;
     @Column(name = "1h-17h")
-    private int h12;
+    private int h1719;
     @Column(name = "1h-19h")
-    private int h12;
+    private int h1921;
     @Column(name = "1h-21h")
-    private int h12;
+    private int h2123;
     @Column(name = "1h-24h")
-    private int h12;
+    private int h1224;
 
 
     @ManyToOne
